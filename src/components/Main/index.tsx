3da1d4e5dd2ba37ccbe0fe539ae7e133a631bec5
brlaney <brlaney@outlook.com>
2022-07-23 08:02:41 -0400

    sat morning additions

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,17 @@
-import boxes from '@/lib/data/boxes';
+// import { useState } from 'react';
+// import boxes from '@/lib/data/boxes';
 import styles from '@/styles/components/Main.module.scss';
 
-
-
 export default function Main() {
+  // const [boxes, setBoxes] = useState(Array(30).fill(null));
+
   return (
     <div className={styles['main-container']}>
-      5 x 6 grid
+      <div className={styles.grid}>
+        {Array(30).fill(null).map((_, i) => (
+          <div key={i} className={styles.box} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,17 @@ import styles from '@/styles/components/Navbar.module.scss';
 export default function Navbar() {
   return (
     <div id={styles.navbar} className={styles['nav-container']}>
-      navbar
+      <div className={styles['nav-row']}>
+        <div id={styles.menu} className={styles['nav-section']}>
+          lhs icons
+        </div>
+        <div id={styles.title} className={styles['nav-section']}>
+          Wordle
+        </div>
+        <div id={styles.options} className={styles['nav-section']}>
+          rhs icons
+        </div>
+      </div>
     </div>
   )
 };
