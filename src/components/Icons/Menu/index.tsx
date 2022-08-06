47a25f417e032cacaab90ada65c60323f9be4fe7
brlaney <brlaney@outlook.com>
2022-08-06 12:48:23 -0400

    More layout improvements to navbar and body boxes

--- src/components/Icons/Menu/index.tsx
+++ src/components/Icons/Menu/index.tsx
@@ -2,29 +2,29 @@ export default function Menu({ display }) {
   return (
     <>
       <svg
-        width="28"
-        height="28"
-        viewBox="0 0 22 22"
+        width="22"
+        height="62"
+        viewBox="0 0 22 62"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M3.5 17H18.5"
+          d="M3.5 37H18.5"
           stroke="white"
-          strokeWidth="2"
-          strokeLinecap="square"
+          stroke-width="2"
+          stroke-linecap="square"
         />
         <path
-          d="M3.5 11H18.5"
+          d="M3.5 31H18.5"
           stroke="white"
-          strokeWidth="2"
-          strokeLinecap="square"
+          stroke-width="2"
+          stroke-linecap="square"
         />
         <path
-          d="M3.5 5H18.5"
+          d="M3.5 25H18.5"
           stroke="white"
-          strokeWidth="2"
-          strokeLinecap="square"
+          stroke-width="2"
+          stroke-linecap="square"
         />
       </svg>
     </>
